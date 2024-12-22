d349b887485459bed1e7f04df2595db055383ee0
Hizkia-wa <siahaanhizkia06@gmail.com>
2024-12-22 16:43:11 +0700

    iru lah itu lihat sendiri lah perubahannya capek aku menjelaskan

--- src/pages/LatihanKimia12.js
+++ src/pages/LatihanKimia12.js
@@ -44,7 +44,7 @@ const Materi = () => {
       <div className="biologi-biology-section">
         <div className="container">
           <div className="section-header">
-            <h2>Materi FISIKA</h2>
+            <h2>Soal Latihan Kimia 12</h2>
           </div>
           <div className="biology-grid">
             {biologies.map((biology) => (
